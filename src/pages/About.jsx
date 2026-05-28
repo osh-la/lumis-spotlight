e55136fd07cc0ec0511321278eaf9eb66b06b3d6
abimiku oshela <abimikuoshela@gmail.com>
2026-05-28 15:21:54 +0100

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,14 +19,13 @@ export default function About() {
           start: "center center",
           end: "bottom bottom",
           scrub: true,
-    
         },
       });
 
       tl.fromTo(
         growRef.current,
         { scaleX: 0.1, transformOrigin: "left center" },
-        { scaleX: 1, ease: "none", duration: 1 }
+        { scaleX: 1, ease: "none", duration: 1 },
       ).fromTo(
         textsRef.current,
         { xPercent: -50, opacity: 0 },
@@ -37,7 +36,7 @@ export default function About() {
           ease: "power2.out",
           duration: 0.75,
         },
-        ">-0.4"
+        ">-0.4",
       );
     }, sectionRef);
 
@@ -45,30 +44,40 @@ export default function About() {
   }, []);
 
   return (
-    <section ref={sectionRef} className="mt-20 md:px-10 text-black space-y-16 overflow-x-hidden">
+    <section
+      ref={sectionRef}
+      className="mt-20 md:px-10 text-black space-y-16 overflow-x-hidden"
+    >
       <section>
-        <h1 className="text-4xl md:text-6xl font-bold mb-8 text-center md:text-left">ABOUT US</h1>
+        <h1 className="text-4xl md:text-6xl font-bold mb-8 text-center md:text-left">
+        Our Little Story
+        </h1>
         <div className="flex flex-col md:flex-row gap-6 items-center">
-          <img
-            className="w-full md:w-2/5 rounded-lg"
-            src="/images/lumi2.jpg"
-            alt="Chairs"
-          />
+          <video
+            className="w-full md:w-2/5 rounded-lg object-cover"
+            src="/images/ab1.mp4"
+            autoPlay
+            muted
+            loop
+            playsInline
+          >
+            Your browser does not support the video tag.
+          </video>
           <div className="flex flex-col gap-4 text-base md:text-lg text-center md:text-left">
             <p>
-              It is more than just furniture; it is stories filled with
-              inspiration, creativity and passion that will bring life to your
-              home.
+              Me i dont know the story oo, but if you let me write something random i think i will just do that in the next paragraph and you asked when i was talking to my self
+              in your dm if it gets crazier, well i guess you are about to find out.
             </p>
             <p>
-              The perfect blend of elegance, practicality and essence with a
-              sense of style for whatever space you want it in.
+              It all started when i saw this tall dark very handsome and good looking guy walking towards me at AYA, the aura of his presence blew me right away! i didnt know who this was but i knew i wanted to see him again,
+             but i am very shy and definitly wasnt going to stop him or ask for his name or his number but i knew i wouldnt be happy if i didnt get to see him again, he was getting closer, i knew i had to think and act fast, so i took out my phone and started making a video of him as he walked towards me! the closer he got the more angles i changed
+             so i could capture all his essence and more importantly not get caught while at it, when he got where i was he looked me in the eyes, help my arms and said "hi, im oshla..i know what you were doing, i hope it turns out as great as you hope" and he walked away majestically, something told me this guy was a prince and yes he smelled amazing too!
+             when i got home i watched those videos and omg! it was the best thing i had seen all day!it was in that moment i realized i was good at this and i decided i was going to make the best out of this and well, as thy say.. the rest is history.
             </p>
           </div>
         </div>
       </section>
 
-     
       <section
         ref={growRef}
         className="bg-red-50 min-h-screen flex flex-col items-center justify-center px-4 py-16 md:px-10 overflow-hidden"
@@ -85,8 +94,8 @@ export default function About() {
         >
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
           harum ipsam officiis blanditiis debitis eius, iure doloremque
-          perferendis nulla possimus, natus error numquam culpa magnam inventore,
-          quo odit fuga reprehenderit.
+          perferendis nulla possimus, natus error numquam culpa magnam
+          inventore, quo odit fuga reprehenderit.
         </p>
         <p
           ref={addTextRef}
@@ -97,6 +106,11 @@ export default function About() {
           dignissimos quos odit? Saepe omnis similique perspiciatis incidunt
           architecto officiis ratione!
         </p>
+         <Link to="/booking">
+            <button className="rounded-full bg-black text-white px-8 py-4 text-sm md:text-base uppercase">
+              Book a Session
+            </button>
+          </Link>
       </section>
     </section>
   );
