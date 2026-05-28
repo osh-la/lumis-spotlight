ee3ac2855506e54bda441bfa1c0d03e5d58af26f
abimiku oshela <abimikuoshela@gmail.com>
2026-05-28 19:26:16 +0100

    logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,14 +5,19 @@ const Nav = ({ onCartClick }) => {
     <nav className="fixed top-0 w-full z-50 bg-transparent shadow-sm text-gray-800 text-base sm:text-xl px-4 sm:px-8 py-4">
       <div className="flex items-center justify-between">
         <div className="font-bold">
-          <TransitionLink to="/">LOGO</TransitionLink>
+          <TransitionLink to="/">
+            <img
+              src="/images/logo.PNG"
+              alt="Logo"
+              className="h-12 w-auto object-contain md:h-16"
+            />
+          </TransitionLink>
         </div>
 
         <div className="flex gap-4">
           <span className="cursor-pointer hover:underline">
             <TransitionLink to="/gallery">Gallery</TransitionLink>
           </span>
-        
         </div>
       </div>
     </nav>
