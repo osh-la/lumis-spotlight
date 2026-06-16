18ba80ad49dc363f960bde55a128c3edaf305d18
abimiku oshela <abimikuoshela@gmail.com>
2026-06-16 15:47:56 +0100

    onboard button

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -675,7 +675,7 @@ export default function OnboardingForm() {
             >
               {loading
                 ? "Submitting..."
-                : "Submit Form"}
+                : "Submit"}
             </button>
 
             <p className="text-[12px] text-[#8a8a84] text-center">
